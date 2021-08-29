49dacf7acd47326973f0b4a72fc26fb78f2872ba
Huiran Lin <hugh80082@gmail.com>
2021-08-29 00:51:04 -0700

    remove gitignore files

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+
+// Import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+// Import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Create a new Apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Apply the Apollow server to the Express server as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
