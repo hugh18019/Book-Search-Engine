9d5e42def5d60f2ab6045de73afee19c7fa1b454
Huiran Lin <hugh80082@gmail.com>
2021-09-03 11:41:01 -0700

    deploying

---
+++ client/build/precache-manifest.d4a7aef8d5fbdfd5ac251f6fc902c712.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd570a7a11eae415d5defef800c1f5c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68ef306f9dd0280b01d0",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "17c3d26092e2b2a7ed2c",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "68ef306f9dd0280b01d0",
+    "url": "/static/js/2.fc0c3000.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.fc0c3000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c3d26092e2b2a7ed2c",
+    "url": "/static/js/main.74554dd8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
